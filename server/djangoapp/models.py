303aa6d965f91e192d558bd5aafc7c9cf0362546
UmamiAppearance <mail@umamiappearance.eu>
2022-03-09 00:01:57 +0100

    models/admin site

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,24 +1,46 @@
+from itertools import product
 from django.db import models
 from django.utils.timezone import now
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=48)
+    description = models.TextField()
+    founded = models.DateField()
+    headquarters = models.CharField(max_length=48)
+    products = models.CharField(max_length=48)
+    parent = models.CharField(max_length=48)
+
+    def __str__(self):
+        return f'#{self.id}: {self.name}'
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name="models")
+    name = models.CharField(max_length=48)
+    dealer_id = models.IntegerField()
+    TYPES = (
+        ("SEDAN", "Sedan"),
+        ("COUPE", "Coupe"),
+        ("SPORTS", "Sports Car"),
+        ("WAGON", "Station Wagon"),
+        ("HATCHBACK", "Hatchback"),
+        ("CONVERTIBLE", "Convertible"),
+        ("SUV", "Sport-Utility Vehicle (SUV)"),
+        ("MINIVAN", "Minivan"),
+        ("PICKUP", "Pickup Truck")
+    )
+    type = models.CharField(blank=True, choices=TYPES, max_length=11)
+    year = models.IntegerField()
+    fuel_consumption = models.IntegerField()
+    efficiency_class = models.CharField(max_length=24)
+
+    def __str__(self):
+        return f'#{self.id}: {self.name}, type: {self.type}'
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
