a94fe73f83aa18d13253f2249dbcd2907e042187
UmamiAppearance <mail@umamiappearance.eu>
2022-03-08 12:20:54 +0100

    node cloud function

---
+++ functions/sample/nodejs/dealerships.js
@@ -0,0 +1,62 @@
+const Cloudant = require('@cloudant/cloudant');
+
+function compileResponse(msg, statusCode=400) {
+    const response = {
+        headers: { 
+            "Content-Type": "application/json"  
+        }, 
+        statusCode: String(statusCode)
+    }
+    if (statusCode !== 400) {
+        response.data = { error: msg };
+    } else {
+        response.data = msg;
+    }
+    return response;
+}
+
+async function main(params) {
+    const cloudant = Cloudant({
+        url: params.COUCH_URL,
+        plugins: {
+            iamauth: {
+                iamApiKey: params.IAM_API_KEY
+            }
+        }
+    });
+    const dealerships_db = cloudant.db.use("dealerships")
+    let query;
+    const fields = ["id", "city", "state", "st", "address", "zip", "lat", "long"];
+    const stateFilter = params.hasOwnProperty("state");
+    if (stateFilter) {
+        query = {
+            selector: {
+                st: {"$eq": params.state },
+            },
+            fields: fields
+        };
+    } else {
+        query = {
+            selector: {},
+            fields: fields
+        };
+    }
+    let dealershipsList;
+    if (stateFilter && !params.state) {
+        dealershipsList = { docs: [] };
+    } else {
+        try {
+            dealershipsList = await dealerships_db.find(query);
+        } catch (error) {
+            return compileResponse(error.description, 500);
+        }
+    }
+    let response;
+    if (dealershipsList.docs.length) {
+        response = compileResponse(dealershipsList.docs);
+    } else {
+        const message = (stateFilter) ? "The state does not exist" :  "The database is empty";
+        response = compileResponse(message, 404);
+    }
+    return response;
+}
